9177aa61c7074705143237f9dc8281d33ad7fb29
CloudLun <cloud830902@gmail.com>
2024-04-25 18:17:06 +0800

    change ' to &apos;

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
                     <p>HaoChe Hung</p>
                 </div>
                 <p className='text-[1rem] text-white'>
-                    Navigating New York City after dark presents a challenge, especially for the city's vibrant youth who revel in its nightlife. As nightlife hotspots and affordable residential areas diverge from the city center, relying on central public transit like the subway becomes impractical. Nighttime taxi rides emerge as the primary alternative for post-party journeys.
+                    Navigating New York City after dark presents a challenge, especially for the city&apos;s vibrant youth who revel in its nightlife. As nightlife hotspots and affordable residential areas diverge from the city center, relying on central public transit like the subway becomes impractical. Nighttime taxi rides emerge as the primary alternative for post-party journeys.
                     This thesis proposes a navigational board to analyze night taxi ridership, mapping routes from nightlife clusters to young residential areas. By assessing demand, cost, and time, this tool aids the NYC Office of Nightlife in prioritizing transit improvements. It empowers the younger generation to navigate nocturnal limitations, easing nighttime mobility struggles in the city.
                 </p>
             </div>
